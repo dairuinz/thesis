106dfc59d27c4adc8696d7ec8f2863470074d386
dairuinz <ormaraziotis@gmail.com>
2023-03-18 17:23:41 +0200

    word2vec

--- alloc.py
+++ alloc.py
@@ -24,62 +24,6 @@ def alloc(df):
     alloc_df['review'] = [re.sub(r'\s+', ' ', sent) for sent in alloc_df['review']]
     alloc_df['review'] = [re.sub(r"\'", "", sent) for sent in alloc_df['review']]
 
-    # alloc_df['review'] = alloc_df['review'].apply(lambda x: ' '.join([w for w in x.split() if len(w) > 3]))
-    #
-    # from nltk.tokenize import RegexpTokenizer
-    # tokenizer = RegexpTokenizer(r'\w+')
-    #
-    # # lower = alloc_df['review'].str.lower()
-    # lower = alloc_df['review'].map(lambda x: x.lower())
-    #
-    # from stop_words import get_stop_words
-    # en_stop = get_stop_words('en')
-    #
-    # for word in lower:
-    #     for w in word.split():
-    #         for k in en_stop:
-    #             if k == w:
-    #                 lower = lower.str.replace(str(w), '')
-    # # print(type(lower))
-    # print(lower)
-    # lower = pd.DataFrame(lower)
-    #
-    # # tokens = lower.apply(lambda x: x.split())
-    # tokens = lower.apply(lambda sentence: [tokenizer.tokenize(word) for word in sentence])
-    # print(tokens)
-    #
-    # # for t in tokens:
-    # #     print(t)
-    # # print(tokens)
-    # # filtered_sentence = [i for i in tokens if not i in en_stop]
-    #
-    # from nltk.stem.porter import PorterStemmer
-    # p_stemmer = PorterStemmer()
-    #
-    # filtered_sentence = lower.apply(lambda x: x.split())
-    # filtered_sentence = filtered_sentence.apply(lambda x: ' '.join([w for w in x if len(w) > 3]))
-    #
-    # # print(filtered_sentence)
-    #
-    # texts = filtered_sentence.apply(lambda sentence: [p_stemmer.stem(word) for word in sentence])
-    #
-    # # for t in texts:
-    # #     print(t)
-    # # print(texts)
-    #
-    # from gensim import corpora, models
-    #
-    # dictionary = corpora.Dictionary(texts)
-    #
-    # corpus = [dictionary.doc2bow(text) for text in texts]
-    #
-    # # print(corpus[0])
-    #
-    # ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=3, id2word=dictionary, passes=20)
-    #
-    # print(ldamodel.print_topics(num_topics=3, num_words=3))
-
-
     alloc_df['processed'] = alloc_df['review'].map(lambda x: re.sub('[,\.!?]', '', x))
     alloc_df['processed'] = alloc_df['review'].map(lambda x: x.lower())
 
@@ -130,5 +74,22 @@ def alloc(df):
     lda_model = gensim.models.LdaMulticore(corpus=corpus,
                                            id2word=id2word,
                                            num_topics=num_topics)
-    pprint(lda_model.print_topics())
+    # pprint(lda_model.print_topics())
     doc_lda = lda_model[corpus]
+
+    # print(alloc_df['processed'])
+    # print(corpus[:1][0][:30])
+    # print(len(corpus[:1][0][:30]))
+
+    # pyLDAvis.enable_notebook()
+
+    # vis = pyLDAvis.prepare(lda_model, corpus, id2word, mds='mmds', R=30)
+    # print(vis)
+    # vis
+    #
+    # plt.figure(figsize=(15, 8))  # size of graph
+    # plt.imshow(vis, interpolation='bilinear')  # puts wordcloud into image
+    # plt.axis('off')  # no axis
+    # plt.show()
+
+    return corpus
